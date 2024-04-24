4dcb51d543c2cae956ddf48853141e4833243c45
darvfus <romerondan123@gmail.com>
2024-04-24 16:13:34 -0600

    "media-Query"

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var yearSpan = document.getElementById("year");
-    var lastModifiedParagraph = document.getElementById("lastModified");
-    
-    var ultimaModificacion = new Date(document.lastModified);
-    var fechaFormato = ultimaModificacion.toLocaleString('es-ES', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'});
+  var yearSpan = document.getElementById("year");
+  var lastModifiedParagraph = document.getElementById("lastModified");
   
-    lastModifiedParagraph.textContent = "lastModified: " + fechaFormato;
-  
-    var year = new Date().getFullYear();
-    yearSpan.textContent = year;
-  });
+  var ultimaModificacion = new Date(document.lastModified);
+  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+  var fechaFormato = ultimaModificacion.toLocaleString('en-US', options);
+
+  lastModifiedParagraph.textContent = "Last Modified: " + fechaFormato;
+
+  var year = new Date().getFullYear();
+  yearSpan.textContent = year;
+});
